bcccc7ced87cdf80bebf3ca9a28412ec12482c2e
Stefan Lamm <s.lamm@andersundsehr.com>
2020-08-05 11:34:14 +0200

    If the alive could not be sent, throw an exception

--- src/HeartBeat.php
+++ src/HeartBeat.php
@@ -15,6 +15,10 @@ class HeartBeat
     public function alive(string $url): void
     {
         $headers = get_headers($url);
+        if (false === $headers) {
+            throw new HeartBeatException('HeartBeat error occurred sending alive');
+        }
+
         foreach ($headers as $header) {
             if ($header === 'HTTP/1.1 200 OK') {
                 return;
